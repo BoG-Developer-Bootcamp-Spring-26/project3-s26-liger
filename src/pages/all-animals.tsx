fd7cb0766c16bc610d54b72f7fe4f6856126c23b
sanyue <liang452@purdue.edu>
2026-04-15 23:19:58 -0400

    all animals page

--- src/pages/all-animals.tsx
+++ src/pages/all-animals.tsx
@@ -1,16 +1,122 @@
+"use client";
 import { useState, useEffect } from "react";
 import AnimalCard from "../components/animalCard";
 import { Sidebar } from "@/components/sidebar";
 import CreateNewButton from "@/components/createNew";
+import { useRouter } from "next/router";
+
 
 export default function AllAnimals() {
-    const [animals, setAllAnimals] = useState<any>(null);
-    return(
+    const [animals, setAnimals] = useState<any[]>([]);
+      const [loading, setLoading] = useState(true);
+      const limit = 25;
+      const [user, setUser] = useState<any>(null);
+      const router = useRouter();
+      const [lastAnimalId, setLastAnimal] = useState('start');
+      const [page, setPageNum] = useState(1);
+      useEffect(() => {
+        const fetchUser = async() =>{
+            try {
+                const res = await fetch("/api/me");
+                const data = await res.json();
+                if (res.ok) {
+                    setUser(data.user);
+                } else {
+                  router.push('/login');
+                }
+            }
+            catch(e) {
+                console.error(e);
+            }
+        }
+        fetchUser();
+      }, []);
+    
+       
+      useEffect(() => {
+        if (!user) {
+            return;
+        }
+        if (!user.isAdmin) {
+            router.push('/animal');
+            
+        }
+         const fetchAnimals = async () => {
+          try {
+            const res = await fetch(`/api/admin/animals/?cursor=${lastAnimalId}&limit=${limit}`);
+            if (res.ok) {
+                const data = await res.json();
+                setAnimals(data.animals || []);
+                setLoading(false);
+                setLastAnimal(data.animals[data.animals.length - 1]._id);
+            } else  {
+                const error = await res.json();
+                console.log(error.message);
+            }
+          } catch (e) {
+            console.error(e);
+          } 
+        };
+      fetchAnimals();
+      }, [user, page]);
+    
+      if (loading) return <div></div>;
+    
+      if (!animals || animals.length === 0) {
+        return (
+         <div className="flex flex-row h-screen w-screen">
+                <Sidebar currentPage="all-animals" user={user.fullName} isAdmin={user.isAdmin}/>
+                <div className="flex flex-col flex-1 px-6">
+                <div className="header">
+                        <h1>All animals</h1>
+                        
+                    </div>
+                    <hr></hr>
+                <div>
+                    <p className="margins-10px">There are no animals.</p>
+                </div>
+
+               <div className="flex items-end justify-center">
+                <button onClick={() => {if(page > 1){setPageNum(page - 1)}}}> ← </button>
+                <p> {page} </p>
+                <button onClick={() => { if (animals.length === limit) setPageNum(page + 1)}}> → </button>
+            </div>
+
+                </div>
+            </div>
+          );
+      }
+
+      return (
         <div className="flex flex-row h-screen w-screen">
-        <Sidebar currentPage="all-animals" user="Long Lam" isAdmin={true}/>
-        <div className="flex flex-1 items-center">
-            display all animals here
+          <Sidebar currentPage={"all-animals"} user={user.fullName} isAdmin={user.isAdmin}></Sidebar>
+          
+          <div className="flex flex-col flex-1 px-6">
+            <div className="header">
+                <p style={{fontSize:'20px'}}>All animals</p>
+            </div>
+    
+            <hr></hr>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 py-8">
+                {animals.map((animal: any) => (
+                <AnimalCard
+                key={animal._id}
+                name={animal.name}
+                breed={animal.breed}
+                hours={animal.hoursTrained}
+                animalpfp={animal.profilePicture}
+                userpfp={""}
+                />
+            ))}
+            </div>
+
+            <div className="flex items-end justify-center">
+                <button onClick={() => {if(page > 1){setPageNum(page - 1)}}}> ← </button>
+                <p> {page} </p>
+                <button onClick={() => { if (animals.length === limit) setPageNum(page + 1)}}> → </button>
+            </div>
+
+          </div>
         </div>
-        </div>
-    );
-}
+      );
+    }
